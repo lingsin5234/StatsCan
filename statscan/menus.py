2b39daf99956b123a0c185c08dd412e4fcc48bde
lingsin5234 <sinto.005234@gmail.com>
2020-08-21 08:16:02 -0600

    updated menu and project page

--- statscan/menus.py
+++ statscan/menus.py
@@ -5,5 +5,5 @@ from . import views as statscan_vw
 
 # add items to the menu
 Menu.add_item("statscan", MenuItem("My Portfolio", url="/", weight=10))
-Menu.add_item("statscan", MenuItem("Stats Can Project", reverse(statscan_vw.project_markdown), weight=10))
+Menu.add_item("statscan", MenuItem("Project Info", reverse(statscan_vw.project_markdown), weight=10))
 Menu.add_item("statscan", MenuItem("GHG Emissions Chart", reverse(statscan_vw.plotGHGEmissions), weight=10))
